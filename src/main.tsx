ff7ab69deb6993a9ccfeb733e77b657c24608e1b
uescobar <uescobar.360@gmail.com>
2026-04-20 15:22:34 -0600

    configuración

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+import App from "./App.tsx";
 
-createRoot(document.getElementById('root')!).render(
+//const router = createBrowserRouter([{ path: "/", element: <>Hola Mundo!</> }]);
+const router = createBrowserRouter([
+  { path: "/", element: <App /> },
+  { path: "/producto", element: <>mis productos</> },
+]);
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
-)
+);
