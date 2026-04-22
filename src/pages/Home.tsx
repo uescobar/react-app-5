4c4b675a30846b6a35a7e2701c19fc506b297f01
uescobar <uescobar.360@gmail.com>
2026-04-22 13:09:29 -0600

    autorizacion

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,7 @@
 import { useNavigate } from "react-router-dom";
 
 export default function Home() {
-  throw new Error("Error! Chanchito triste :(");
+  // throw new Error("Error! Chanchito triste :(");
   const navigate = useNavigate();
   return (
     <>
--- src/pages/Product.tsx
+++ src/pages/Product.tsx
@@ -1,8 +1,16 @@
-import { Link } from "react-router-dom";
+import { Link, Navigate } from "react-router-dom";
 
 type Props = {};
 
+const useUser = () => undefined;
+
 function Product({}: Props) {
+  const user = useUser();
+
+  if (!user) {
+    return <Navigate to="/" />;
+  }
+
   const products = [
     { id: 1, name: "iPhone" },
     { id: 2, name: "Android" },
